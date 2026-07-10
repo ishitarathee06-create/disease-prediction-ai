2bdf601e670229d90237663edcff980343d2faee
ishitarathee06-create <ishitarathee06@gmail.com>
2026-07-10 12:42:35 +0530

    Update nlpe.py

--- nlpe.py
+++ nlpe.py
@@ -4,7 +4,12 @@ from spacy.matcher import PhraseMatcher
 from rapidfuzz import process, fuzz
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 
 class SymptomExtractor:
